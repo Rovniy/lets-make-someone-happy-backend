2f004806d1aceece28f27a7774d0c9da3dd96699
Rovniy <smart.fz@mail.ru>
2022-07-12 13:54:13 +0300

    MVP 1.6 api_prefix

--- utils/constants.js
+++ utils/constants.js
@@ -2,5 +2,6 @@ module.exports = {
 	isProduction: process.env.NODE_ENV === 'production',
 	isDevelopment: process.env.NODE_ENV === 'development',
 	seeds_cycles: 10,
-	initial_value: 1
+	initial_value: 1,
+	api_prefix: '/api'
 }
